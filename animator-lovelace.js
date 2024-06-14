1142a527f427a4b1255227e550630495de7a8260
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-06-14 10:50:35 -0300

    body to window

--- animator-lovelace.js
+++ animator-lovelace.js
@@ -1,34 +1,38 @@
-document.body.addEventListener("load", function () {
-  const targets = document.getElementsByClassName("bg-animated-lovelace");
+window.addEventListener(
+  "load",
+  function () {
+    const targets = document.getElementsByClassName("bg-animated-lovelace");
 
-  for (let i = 0; i < targets.length; i++) {
-    animate(targets[i]);
-  }
-
-  function animate(target) {
-    target.style.backgroundImage = "url(./imagens/background-animated.jpeg)";
-    target.style.backgroundSize = "cover";
-    target.style.backgroundPosition = "center top";
-    target.style.backgroundRepeat = "no-repeat";
-
-    if (target.style.transition === "") {
-      target.style.transition = "background-position .8s";
-    } else {
-      target.style.transition += ", background-position .8s";
+    for (let i = 0; i < targets.length; i++) {
+      animate(targets[i]);
     }
 
-    target.onmouseenter = function () {
-      target.style.backgroundPosition = "center bottom";
-    };
-    target.onfocus = function () {
-      target.style.backgroundPosition = "center bottom";
-    };
+    function animate(target) {
+      target.style.backgroundImage = "url(./imagens/background-animated.jpeg)";
+      target.style.backgroundSize = "cover";
+      target.style.backgroundPosition = "center top";
+      target.style.backgroundRepeat = "no-repeat";
 
-    target.onmouseleave = function () {
-      target.style.backgroundPosition = "center top";
-    };
-    target.onblur = function () {
-      target.style.backgroundPosition = "center top";
-    };
-  }
-});
+      if (target.style.transition === "") {
+        target.style.transition = "background-position .8s";
+      } else {
+        target.style.transition += ", background-position .8s";
+      }
+
+      target.onmouseenter = function () {
+        target.style.backgroundPosition = "center bottom";
+      };
+      target.onfocus = function () {
+        target.style.backgroundPosition = "center bottom";
+      };
+
+      target.onmouseleave = function () {
+        target.style.backgroundPosition = "center top";
+      };
+      target.onblur = function () {
+        target.style.backgroundPosition = "center top";
+      };
+    }
+  },
+  false
+);
